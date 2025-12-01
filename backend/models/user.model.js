2b56ad47f352fb8b47c32b2502456dcf5b1397a0
Neon2063 <kcprabin2063@gmail.com>
2025-12-01 11:16:13 +0545

    Made User Model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,7 +1,27 @@
 import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema({
+    username:{
+        type:String,
+        required:true,
+        lowercase:true,
+        trim:true,
+        index:true,
+    },
+    Name:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    Password:{
+        type:String,
+        required:true,
+    },
+    kuid:{
+        type:Number,
+        required:true,
+    },
 
 },{timestamps:true})
 
-
+export const User = mongoose.model("user",userSchema)
